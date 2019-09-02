9b047d0f1f1bd253e20b406e8933f24e0c727d6a
nicolemistry <nicolemistry@nics-macbook.wireless-1x.unc.edu>
2019-09-02 03:12:25 -0400

    finished jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,14 +1,52 @@
 package a1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class A1Jedi {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
+Scanner key = new Scanner(System.in);
+		
+		//global 
+		int numberOfItemsInStore = key.nextInt();
+		String[] items = new String[numberOfItemsInStore];
+		int[] itemCounts = new int[numberOfItemsInStore];
+		int[] itemCustomerCounts = new int[numberOfItemsInStore];
+	
+		for(int p = 0; p < numberOfItemsInStore; p++) {
+			items[p] = key.next();
+			//takes in the data, but doesn't do anything with it
+			double itemPrice = key.nextDouble();
+		}
+		
+		int numberOfCustomers = key.nextInt();
+	
+		for(int i = 1; i <= numberOfCustomers; i++) {
+			//takes in the data, but doesn't do anything with it
+			String firstName = key.next();
+			String lastName = key.next();
+			int numberOfItemsBought = key.nextInt();
 
-		// Your code follows here.
+			for(int n = 1; n <= numberOfItemsBought; n++) {
+				int quantity = key.nextInt();
+				String item = key.next();
+				itemCustomerCounts[Arrays.asList(items).indexOf(item)]+= 1;
+				itemCounts[Arrays.asList(items).indexOf(item)] += quantity;
+			}
+			
+		}
+		
+		for(int m = 0; m < items.length; m++) {
+			
+			if(itemCounts[m] == 0) 
+				System.out.println("No customers bought " + items[m]);
+			else
+				System.out.println(itemCustomerCounts[m] + " customers bought " + itemCounts[m] + " " + items[m]);
+		}
+
+		key.close();		
 		
 	}
 }
